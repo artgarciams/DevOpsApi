38c46efc0784a42cdf1d3a91b1268886f7339822
Art Garcia <45210520+artgarciams@users.noreply.github.com>
2020-10-19 15:13:47 -0400

    Delete Untitled3.ps1

--- Untitled3.ps1
+++
@@ -1,8 +0,0 @@
-﻿$sub = Get-AzureRmSubscription
-$subJson = ConvertTo-Json $sub
-Select-AzureSubscription -SubscriptionId 5812a6d7-817f-4798-b4ad-8a991a941edd
-
-$grp = Get-AzureRmResourceGroup
-$json = ConvertTo-Json $grp
-
-write-host $json
